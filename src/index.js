f99c5354f4876c178378de30d026480da9cca570
bera5186 <rbasu611@gmail.com>
2019-09-05 13:23:04 +0530

    Created Task model and basic Task creation endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,6 +12,15 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
         res.send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.post('/task', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
